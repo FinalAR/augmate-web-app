8c529c2936e1b43f4b6678e84e946b931d6f18b3
Manuka Dewanarayana <84101425+manudew@users.noreply.github.com>
2024-03-03 13:38:26 +0530

    Content and target upload version 01

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,10 +14,13 @@ const firebaseConfig = {
   storageBucket: "augmate-d844e.appspot.com",
   messagingSenderId: "935985492697",
   appId: "1:935985492697:web:2bd2b66a52a633d70744c8",
-  measurementId: "G-6WS0G7THH5"
+  measurementId: "G-6WS0G7THH5",
+  storageBucket:"gs://augmate-d844e.appspot.com"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage();
+const auth = getAuth(app);
 
-export const auth = getAuth(app);
+export {storage, auth}
