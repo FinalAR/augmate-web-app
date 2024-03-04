736a8fee75561fdabb0c56a4998b94290131f6f5
= <pamodhamahagamage98@gmail.com>
2024-03-04 10:48:53 +0530

    Feat-Add-QR Scammer

--- src/components/QRScanner.js
+++ src/components/QRScanner.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
-import {QrReader} from 'react-qr-reader';
+import { QrReader } from 'react-qr-reader';
+
+import { Scanner , useDeviceList, useContinuousScanner} from '@yudiel/react-qr-scanner';
 
 import QRCode from 'qrcode.react';
 
 function QRScanner() {
   const [qrData, setQRData] = useState(null);
   const [data, setData] = useState(null);
+
+  const [deviceInput, setInput] = useState(useDeviceList())
+  console.log(useDeviceList());
+
+  console.log(deviceInput);
+  console.log(deviceInput[0]);
 
   const handleScan = (data) => {
     if (data) {
@@ -18,33 +26,48 @@ function QRScanner() {
     console.error(err);
   };
 
-  return (
-  <div>
-  <h1>QR Code Scanner</h1>
-  {data ? (
+    return (
     <div>
-      <p>Scanned Data: {data}</p>
-      <QRCode value={data} />
-    </div>
-  ) : (
-    <QrReader
-      delay={300}
-      onError={handleError}
-      onScan={handleScan}
-      onResult={(result, error) => {
-      if (!!result) {
-        setData(result?.text);
-      }
+    <h1>QR Code Scanner</h1>
+    {data ? (
+      <div>
+        <p>Scanned Data: {data}</p>
+        <QRCode value={data} />
+      </div>
+    ) : (
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        onResult={(result, error) => {
+        if (!!result) {
+          setData(result?.text);
+        }
 
-      if (!!error) {
-        console.info(error);
-      }
-    }}
-      style={{ width: '100%' }}
-    />
-  )}
-</div>
-  );
+        if (!!error) {
+          console.info(error);
+        }
+      }}
+        style={{ width: '100%' }}
+      />
+    )}
+  </div>
+    );
+
+  // return (
+  // <>
+  // {/* <Scanner
+  //   onResult={(text, result) => console.log(text, result)}
+  //   onError={(error) => console.log(error?.message)}
+  //   enabled= {true}
+  //   options= {"b6be7fff0dbb28f522ead65d0d80f035fe8a7833e07fe8fc18c23594d464e57d"}
+  // /> */}
+  // {/* <useContinuousScanner 
+  //   onResult={(text, result) => console.log(text, result)}
+  //   onError={(error) => console.log(error?.message)}
+  // /> */}
+  // </>
+  // )
 }
 
 export default QRScanner;
