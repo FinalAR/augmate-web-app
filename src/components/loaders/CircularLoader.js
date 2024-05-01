d83898d167eeb63a475000d5d5964ff4d07a6517
= <pamodhamahagamage98@gmail.com>
2024-05-01 07:32:30 +0530

    Feat_Add-Circular Progress Fix

--- src/components/loaders/CircularLoader.js
+++ src/components/loaders/CircularLoader.js
@@ -4,18 +4,26 @@ import "react-circular-progressbar/dist/styles.css";
 import "../../assets/css/loader.css";
 
 const CircularLoading = ({ loading, color, progress }) => {
+
+  const roundedProgress = Math.floor(progress)
+
   return (
     <div className="loader-container">
       {loading && (
         <CircularProgressbar
           value={progress}
-        //   text={`${progress}%`}
+          text={`${roundedProgress}%`} 
+          strokeWidth={8} 
+          //text={`${progress}%`}
           styles={{
             path: {
               stroke: color || "#ffffff"
             },
             text: {
-              fill: color || "#ffffff"
+              fill: color || "#ffffff",
+              fontFamily: "Arial, sans-serif", 
+              fontSize: "18px", 
+              fontWeight: "bold" 
             }
           }}
         />
